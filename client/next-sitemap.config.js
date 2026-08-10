0616f399912d170ad79c7edc9608ee894e629f94
Harshad <harshad7237@gmail.com>
2026-08-10 15:09:01 +0530

    fix: update siteUrl to include www subdomain for consistency and correctness

--- client/next-sitemap.config.js
+++ client/next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: 'https://successcodeacademy.in',
+siteUrl: 'https://www.successcodeacademy.in',
   generateRobotsTxt: true, // Automatically generates a robots.txt file
   sitemapSize: 7000,
 }
